0e0457000000bbc855c8f343f6e38fe9e2837ffb
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-08-22 13:56:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-var sayHiToGrandma = "I can\'t hear you!"
+var mixedCase = "I can\'t hear you!"
 
 function sayHiToGrandma(string) {
   if sayHiToGrandma.mixedCase === string.toLowerCase()
