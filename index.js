3b57f245e75f3185ca35589349d7bf992d1bf0ab
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-08-22 13:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function shout(string) {
+  if string.toUpperCasereturn string.toUpperCase()
+}
