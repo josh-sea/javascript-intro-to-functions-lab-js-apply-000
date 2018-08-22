7e7d93c3aecfd64bf7c6bd6afb7c1724950bb6a8
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-08-22 13:44:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function shout(string) {
   return string.toUpperCase()
 }
- 
+
+function whisper(string) {
+  return string.toLowerCase()
+}
 function shout(string) {
   return string.toUpperCase()
 }
